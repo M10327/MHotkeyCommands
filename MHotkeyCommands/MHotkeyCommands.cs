12adc8d547c5d75f20ad46e2c8f5a93db7d2afc9
M10327 <Melody@silverportreborn.com>
2022-11-17 01:46:55 -0600

    basic dynamics added

--- MHotkeyCommands/MHotkeyCommands.cs
+++ MHotkeyCommands/MHotkeyCommands.cs
@@ -3,6 +3,7 @@ using Rocket.API;
 using Rocket.Core;
 using Rocket.Core.Plugins;
 using Rocket.Unturned;
+using Rocket.Unturned.Chat;
 using Rocket.Unturned.Events;
 using Rocket.Unturned.Player;
 using SDG.Unturned;
@@ -12,7 +13,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
+using UnityEngine;
 
 namespace MHotkeyCommands
 {
@@ -82,14 +85,42 @@ namespace MHotkeyCommands
             var cmds = command as List<string>;
             for (int i = 0; i < cmds.Count; i++)
             {
-                cmds[i] = AddDynamics(cmds[i], pl);
+                cmds[i] = AddDynamics(cmds[i], pl, gesture);
             }
             ExecuteCommands(pl, cmds);
         }
 
-        public string AddDynamics(string str, UnturnedPlayer p)
+        public string AddDynamics(string str, UnturnedPlayer p, string gesture)
         {
-            // TODO: dynamics
+            if (p.HasPermission("Binds.Dynamic.GestureName") || p.HasPermission("Binds.Dynamic.*")) str = str.Replace("{G}", gesture);
+            if (p.HasPermission("Binds.Dynamic.Bearing") || p.HasPermission("Binds.Dynamic.*")) str = str.Replace("{B}", Math.Floor(p.Rotation).ToString());
+            if (p.HasPermission("Binds.Dynamic.Caller") || p.HasPermission("Binds.Dynamic.*"))
+            {
+                str = str.Replace("{C.ID}", p.CSteamID.ToString());
+                str = str.Replace("{C.Pos}", p.Position.ToString());
+                str = str.Replace("{C.X}", Math.Round(p.Position.x, 2).ToString());
+                str = str.Replace("{C.Y}", Math.Round(p.Position.y, 2).ToString());
+                str = str.Replace("{C.Z}", Math.Round(p.Position.z, 2).ToString());
+                str = str.Replace("{C.Name}", p.DisplayName);
+            }
+            if (Regex.IsMatch(str, "[{]T[.a-zA-z]*[}]") && (p.HasPermission("Binds.Dynamic.Target") || p.HasPermission("Binds.Dynamic.*"))){
+                int colliders = RayMasks.BARRICADE | RayMasks.STRUCTURE | RayMasks.PLAYER | RayMasks.ENVIRONMENT | RayMasks.GROUND2 | RayMasks.GROUND;
+                Physics.Raycast(p.Player.look.aim.position, p.Player.look.aim.forward, out RaycastHit ray, 500, colliders);
+                Transform target = ray.collider?.transform;
+                List<Player> nearbyPlayers;
+                PlayerTool.getPlayersInRadius(target.position, 0.5f, nearbyPlayers = new List<Player>());
+                if (nearbyPlayers.Count < 1)
+                {
+                    return "ERROR_NOPLAYERFOUND";
+                }
+                else
+                {
+                    UnturnedPlayer t = UnturnedPlayer.FromPlayer(nearbyPlayers.First());
+                    if (t == null) return "ERROR_NOPLAYERFOUND";
+                    str = str.Replace("{T.Name}", t.DisplayName);
+                    str = str.Replace("{T.ID}", t.CSteamID.ToString());
+                }
+            }
             return str;
         }
 
@@ -98,6 +129,11 @@ namespace MHotkeyCommands
             foreach (var command in cmds.ToArray())
             {
                 string cmd = command;
+                if (cmd == "ERROR_NOPLAYERFOUND")
+                {
+                    UnturnedChat.Say(p.CSteamID, "No player found");
+                    continue;
+                }
                 // TODO: make the chat mode default to the player's set chat mode, but this works for now
                 byte mode = (byte)EChatMode.GLOBAL;
                 if (p.HasPermission("Binds.Chatmode"))
